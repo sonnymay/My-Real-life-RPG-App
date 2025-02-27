9d080484d895ed5ece58ea3ca4fb6d6549be61eb
Sonny May <thesan_555@hotmail.com>
2025-02-27 10:54:10 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import RealLifeRPG from "./RealLifeRPG";
+
+export default function App() {
+  return (
+    <div className="App">
+      <RealLifeRPG />
+    </div>
+  );
+}
